fdb78e3f15bd15f1e0cf79b6a9cfea9d0a57d173
Perpetual Akinyi <you@example.com>
2024-10-16 14:18:27 +0300

    linked the files

---
+++ app.js
@@ -0,0 +1,10 @@
+
+const baseUrl = 'http://localhost:3000/results'
+document.addEventListener('DOMContentLoaded',()=>{
+    fetch(baseUrl)
+    .then(response => response.json())
+    .then(data =>{
+        console.log(data)
+    })
+})
+    
