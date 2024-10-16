aa21f77e0bb45e7b0b5fa755d5c96ccdea49a5b3
Perpetual Akinyi <you@example.com>
2024-10-16 15:14:37 +0300

    connected the Data to browser

--- app.js
+++ app.js
@@ -4,8 +4,27 @@ document.addEventListener('DOMContentLoaded',()=>{
     fetch(baseUrl)
     .then(response => response.json())
     .then(data =>{
-        console.log(data)
+        let getQuiz = document.getElementById('get-quiz')
+        data.forEach(quiz =>{
+            displayQuiz(quiz)
+        })
+        
+
     })
     . catch(error => console.log(error))
 })
-    
+function  displayQuiz(quiz){
+    let getQuiz = document.getElementById('get-quiz')
+    let quizContainer = document.createElement('div')
+    quizContainer.innerHTML = `
+    <h2 id='category'>${quiz.category}</h2>
+    <p>Type:${quiz.type}</p>
+    <p>Difficulty:${quiz.difficulty}</p>
+    <p>question:${quiz.question}</p>
+    <button><p>correct_answer:${quiz.correct_answer}</p></button>
+    <br>
+    <button><p>incorrect_answers:${quiz.incorrect_answers}</p></button>
+    `
+    getQuiz.appendChild(quizContainer)
+}
+
