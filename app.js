cae9f9ce11aa97f7248fc134522ef28bb0e1d9fd
Perpetual Akinyi <you@example.com>
2024-10-17 21:26:23 +0300

    I have added a Form

--- app.js
+++ app.js
@@ -3,28 +3,66 @@ const baseUrl = 'http://localhost:3000/results'
 document.addEventListener('DOMContentLoaded',()=>{
     fetch(baseUrl)
     .then(response => response.json())
-    .then(data =>{
-        let getQuiz = document.getElementById('get-quiz')
-        data.forEach(quiz =>{
+    .then(results =>{
+        results.forEach(quiz =>{
             displayQuiz(quiz)
         })
         
 
     })
-    . catch(error => console.log(error))
+    . catch(error => console.log(error));
 })
+
+
 function  displayQuiz(quiz){
     let getQuiz = document.getElementById('get-quiz')
     let quizContainer = document.createElement('div')
+
+    quizContainer.classList.add("quiz-div")
     quizContainer.innerHTML = `
     <h2 id='category'>${quiz.category}</h2>
     <p>Type:${quiz.type}</p>
     <p>Difficulty:${quiz.difficulty}</p>
     <p>question:${quiz.question}</p>
-    <button><p>correct_answer:${quiz.correct_answer}</p></button>
-    <br>
-    <button><p>incorrect_answers:${quiz.incorrect_answers}</p></button>
+    <form id='quiz-form-${quiz.id}'class="quiz-form">
+    <input type="text" id="user-answer-${quiz.id}" placeholder="Enter your answer">
+    <button type="submit">Submit</button>
+    </form>
+    <p id="result-${quiz.id}" class="result"></p></button>
+ 
     `
-    getQuiz.appendChild(quizContainer)
+    getQuiz.appendChild(quizContainer);
+
+const form=  document.querySelector(`#quiz-form-${quiz.id}`);
+       
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const userAnswer = document.getElementById(`user-answer-${quiz.id}`).value;
+        const correctAnswer =  quiz.correct_answer.trim().toLowerCase();
+        const result = document.getElementById(`result-${quiz.id}`);
+
+        if (userAnswer.trim().toLowerCase() === correctAnswer) {
+            result.textContent = `Correct! The incorrect answer is:${quiz.incorrect_answers}`;
+            result.style.color = 'blue'
+            
+        }else {
+            result.textContent = `Incorrect!The correct answer is: ${quiz.correct_answer}`;
+            result.style.color = 'red'
+        }
+form.reset()
+    })
 }
+
+
+
+    
+
+
+ 
+
+ 
+
+
+
+
 
