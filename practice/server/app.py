ddb483187437069897dbde2eda76c59682c45d98
Perpetual Akinyi <you@example.com>
2025-01-20 01:19:38 +0300

    used jsonify to return a request

--- practice/server/app.py
+++ practice/server/app.py
@@ -1,11 +1,11 @@
-from flask import Flask,request
+from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy 
 from flask_migrate import Migrate
 from models import db, Profile
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-app.config['SQLALCHEMY_TRACK_MODERATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app) #connects database to application
 migrate = Migrate(app, db) #connects migration to application
@@ -17,13 +17,19 @@ def index():
 
 @app.route('/profiles/<int:id>')
 def get_profile(id):
-    profile = profile.query.get(id)
+    profile =Profile.query.get(id)
+
+    profile_obj = {
+        'age': profile.age,
+        'email': profile.email,
+        'user_id': profile.user_id
+    }
 
     if profile:
-        return f'Profile: {profile.age} - {profile.email} - {profile.user_id}'
+     return jsonify(profile_obj), 200
+    
+    return jsonify({'message': 'Profile not found'}), 404
 
-    else:
-        return 'Profile not found', 404
 
 
 if __name__ == '__main__':
