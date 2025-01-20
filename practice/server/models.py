3612663325e15b4a67a669c7ee9d2f2b7811b21b
Perpetual Akinyi <you@example.com>
2025-01-20 11:39:29 +0300

    added a tag,assossiation class

--- practice/server/models.py
+++ practice/server/models.py
@@ -21,6 +21,23 @@ class Profile(db.Model):
   email = db.Column(db.String, nullable=False, unique=True)
   user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
   user = db.relationship('User', back_populates='profile')
+  tags= db.relationship('Tag', back_populates='profile',  secondary= 'profile_tag')
 
   def __repr__(self):
    return f'<Profile {self.id}>'
+
+profile_tags = db.Table('profile_tags',
+ db.Column('profile_id', db.Integer, db.ForeignKey('profiles.id'), primary_key=True),
+ db.Column('tag_id', db.Integer, db.ForeignKey('tags.id'), primary_key=True))
+
+class Tag(db.Model):
+  __tablename__ = "tags"
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String, nullable=False)
+
+  profiles =  db.relationship('Profile', secondary= 'profile_tags', back_populates='tags')
+
+def __repr__(self):
+  return f'<Tag {self.id}>'
+
+  
